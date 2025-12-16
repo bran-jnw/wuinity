e420206c11736e920b5c6c545e987a44013ab94a
bran-jnw <jonathan.wahlqvist@brand.lth.se>
2025-12-16 08:20:10 +0100

    1. Non working Canadian fire behavour prediction 2. Generalized spread models through abstract class to allow for any implementation

---
+++ PREACT/PREACTcore/Source/Hazards/Fire/SpreadModels/SpreadModel.cs
@@ -0,0 +1,11 @@
+﻿namespace PREACT.Fire
+{
+    public abstract class SpreadModel
+    {
+        public abstract void DoRun();
+        public abstract double GetMaxSpreadRate();
+        public abstract double GetSpreadRateInDirection(double direction);
+        public abstract double GetMaxSpreadRateDirection();
+        public abstract void SetWind(double direction, double speed);
+    }
+}
